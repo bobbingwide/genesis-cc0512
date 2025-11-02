7edcb45f1441355a8f1b6e1d28606ef732af6c4e
Herb Miller <herb@bobbingwide.com>
2025-11-02 15:13:30 +0000

    Apply Genesis accessibility #12

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'cookie-cat' );
 define( 'CHILD_THEME_URL', 'http://www.bobbingwide.com/oik-themes/genesis-cc0512' );
-define( 'CHILD_THEME_VERSION', '2.1.2' );
+define( 'CHILD_THEME_VERSION', '1.1.1' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
@@ -26,6 +26,17 @@ add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Enable Genesis Accessibility Components
+// See https://my.studiopress.com/documentation/snippets/accessibility/enable-accessibility-features/
+add_theme_support( 'genesis-accessibility', array(
+	'404-page',
+	'drop-down-menu',
+	'headings',
+	'rems',
+	'search-form',
+	'skip-links',
+) );
 
 //add_filter( 'genesis_footer_creds_text', "cookie_cat_footer_creds_text" );
 add_filter( 'genesis_pre_get_option_footer_text', 'cookie_cat_footer_creds_text' );
